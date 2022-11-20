0e9d54c9aa07962f0aa46056e877f3c245df94d8
Radyslav_Shovkivskyi <r.shovkivskyi@gmail.com>
2022-11-20 22:51:16 +0200

    Created NewNote block and Form for it. Created useLS to save all data in LS

---
+++ src/NewNote.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NoteData } from './App'
+import { NoteForm } from "./NoteFrom"
+
+
+type NewNoteProps = {
+  onSubmit: (data: NoteData)=>void
+}
+
+export const NewNote = ({onSubmit}: NewNoteProps ) => {
+  return (
+    <>
+        <h1 className='mb-4'>New Note</h1>
+        <NoteForm  onSubmit={onSubmit} />
+    </>
+  )
+}
