e9758cacd3862363a7c45dd719edee25e253fe95
Radyslav_Shovkivskyi <r.shovkivskyi@gmail.com>
2022-11-21 15:41:50 +0200

    Added delete note logic

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,13 @@ function App() {
     })
   }
 
-  const addTag = (tag: Tag) =>{
+  const onDeleteNote = (id: string) => {
+    setNotes(prevNotes => {
+      return prevNotes.filter(note => note.id !== id)
+    })
+  }
+
+  const addTag = (tag: Tag) => {
     setTags(prev => [...prev, tag])
   }
 
@@ -72,7 +78,7 @@ function App() {
       <Route path='/' element={<NoteList notes={notesWithTags} availableTags={tags} />} />
       <Route path='/new' element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} availableTags={tags} />} />
       <Route path='/:id' element={<NoteLayout notes={notesWithTags} />} >
-        <Route index element={<Note />} />
+        <Route index element={<Note onDelete={onDeleteNote} />} />
         <Route path='edit' element={<EditNote onSubmit={onUpdateNote} onAddTag={addTag} availableTags={tags} />} />
       </Route>
       <Route path='/new' element={<h1>New</h1>} />
