70b42dc98049c44ccef9dcfca665e5a079045dfb
Radyslav_Shovkivskyi <r.shovkivskyi@gmail.com>
2022-11-21 15:12:42 +0200

    Added NoteLayout and Note show page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { NewNote } from './NewNote'
 import { useLocalStorage } from './useLocalStorage'
 import {v4 as uuidV4} from 'uuid'
 import { NoteList } from './NoteList'
+import { NoteLayout } from './NoteLayout'
+import { Note } from './Note'
 
 export type Note = {
   id: string;
@@ -56,8 +58,8 @@ function App() {
     <Routes>
       <Route path='/' element={<NoteList notes={notesWithTags} availableTags={tags} />} />
       <Route path='/new' element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} availableTags={tags} />} />
-      <Route path='/:id'>
-        <Route index element={<h1>Show</h1>} />
+      <Route path='/:id' element={<NoteLayout notes={notesWithTags} />} >
+        <Route index element={<Note />} />
         <Route path='edit' element={<h1>Edit</h1>} />
       </Route>
       <Route path='/new' element={<h1>New</h1>} />
