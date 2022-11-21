7b47cbe2bdd938efe5a8f72bb9462acb35d89acd
Radyslav_Shovkivskyi <r.shovkivskyi@gmail.com>
2022-11-21 10:55:02 +0200

    Created tag logic

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import { NoteData } from './App'
+import { NoteData, Tag } from './App'
 import { NoteForm } from "./NoteFrom"
 
 
 type NewNoteProps = {
   onSubmit: (data: NoteData)=>void
+  onAddTag: (tag: Tag) => void
+  availableTags: Tag[]
 }
 
-export const NewNote = ({onSubmit}: NewNoteProps ) => {
+export const NewNote = ({onSubmit, availableTags, onAddTag}: NewNoteProps ) => {
   return (
     <>
         <h1 className='mb-4'>New Note</h1>
-        <NoteForm  onSubmit={onSubmit} />
+        <NoteForm  onSubmit={onSubmit} onAddTag={onAddTag} availableTags={availableTags} />
     </>
   )
 }
